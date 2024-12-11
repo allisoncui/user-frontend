99ba02c5e5de8dd1d4df13fa282f79afb2d432bb
alyucu6 <allisonycui@gmail.com>
2024-12-11 14:41:50 -0500

    amplify demo

--- user-profile-ui/src/components/RegisterUser.js
+++ user-profile-ui/src/components/RegisterUser.js
@@ -14,7 +14,7 @@ const RegisterUser = () => {
       if (response.status === 201) {
         setMessage("User registered successfully");
       } else if (response.status === 200) {
-        setMessage("User already exists");
+        setMessage("User already exists!!");
       }
     } catch (error) {
       setMessage(error.response.data.detail || "Error registering user");
@@ -31,8 +31,8 @@ const RegisterUser = () => {
     <div>
       {/* <h2>Register User</h2> */}
       <button onClick={handleGoogleLogin} className="google-button">
-      <img src={googleLogo} alt="Google logo" className="google-logo" />
-      <span className="google-button-text">Register with Google</span>
+        <img src={googleLogo} alt="Google logo" className="google-logo" />
+        <span className="google-button-text">Register with Google</span>
       </button>
       <div style={{ paddingTop: '20px' }}>
         <h3>Register New User</h3>
